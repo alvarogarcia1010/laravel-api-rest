0d5d91f6f3e43c68a34f6c36cf809837f767bd23
alvarogarcia1010 <00111317@uca.edu.sv>
2020-10-22 23:16:20 -0600

    Project created

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'sku' => $this->faker->isbn13,
+            'name' => $this->faker->sentence(),
+            'remark' => $this->faker->text(200),
+            'quantity' => $this->faker->randomNumber(2, true),
+            'price' => $this->faker->randomFloat(2, 0, 999),
+            'image_url' => $this->faker->imageUrl(640, 480, 'abstract', true, 'article'),
+        ];
+    }
+}
---
+++ database/migrations/2020_10_23_041154_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku')->nullable()->index();
+            $table->string('name', 100);
+            $table->integer('quantity');
+            $table->double('price', 13,2)->default(0);
+            $table->text('remark')->nullable();
+            $table->text('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
