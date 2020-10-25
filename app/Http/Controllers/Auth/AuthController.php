2787deb4318c2a5f52743e4bb5cc796013f74207
alvarogarcia1010 <00111317@uca.edu.sv>
2020-10-24 22:12:45 -0600

    Fixed bug in AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
 	protected $AuthenticationManagerService;
 
     public function __construct(
-		AuthenticationManagementInterface $AuthenticationManagerService,
+		AuthenticationManagementInterface $AuthenticationManagerService
 	)
 	{
 		$this->AuthenticationManagerService = $AuthenticationManagerService;
